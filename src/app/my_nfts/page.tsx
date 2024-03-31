c8192a7068b566a8e5a562ec0808902a17ca32e8
Sam Acer <sam@sam.fr>
2024-03-31 15:32:07 +0200

    feat: ongoing my_nfts page

--- src/app/my_nfts/page.tsx
+++ src/app/my_nfts/page.tsx
@@ -1,14 +1,5 @@
 'use client';
 
-
-/*
-Bienvenue Samir dans ton objectif d'aujourd'hui, tu vas créer une page pour afficher les NFTs possédés par une adresse Ethereum.
-Tu as creer une page qui contient tout les NFTs possédés par une adresse Ethereum, et un composant qui les affiches.
-Tu en es encore a l'étape ou tu as un truc qui a l'air de se tenir, mais rien qui compile ou quoi.
-Notamment, au niveau des states et des hooks pour avoir toutes les infos.
-Bon courage Samir, tu vas y arriver.
-
-*/
 // pages/nfts.tsx
 import { useState } from 'react';
 import DisplayMyNfts from '../components/DisplayMyNfts';
@@ -16,40 +7,41 @@ import { useWallet } from '../context/WalletContext';
 
 const MyNftsPage: React.FC = () => {
 	const { account } = useWallet();
-  const [ address, setAddress ] = useState<string>(account);
 
-  const [fetchError, setFetchError] = useState<string | null>(null);
+	const [fetchError, setFetchError] = useState<string | null>(null);
 
-  const fetchNFTs = async (): Promise<string[]> => {
-    try {
-      const response = await fetch(`/api/getNFTs?address=${address}`);
-      if (!response.ok) {
-        throw new Error('Failed to fetch');
-      }
-      const data = await response.json();
-      setFetchError(null);
-      return data.tokenIds;
-    } catch (error) {
-      console.error('Error fetching NFTs:', error);
-      setFetchError('Failed to fetch NFTs. Please try again.');
-      return [];
-    }
-  };
+	const fetchNFTs = async (): Promise<string[]> => {
+		try {
+			console.log(account)
+			const response = await fetch(`/api/ether_api/get_all_nfts?address=${account}`);
+			if (!response.ok) {
+				throw new Error('Failed to fetch');
+			}
+			const data = await response.json();
+			setFetchError(null);
+			console.log('NFTs:', data.tokenIds)
+			return data.tokenIds;
+		} catch (error) {
+			console.error('Error fetching NFTs:', error);
+			setFetchError('Failed to fetch NFTs. Please try again.');
+			return [];
+		}
+  	};
 
-  return (
-    <div>
-      <h1>Check NFTs Owned by an Address</h1>
-      <input
-        type="text"
-        value={address}
-        onChange={(e) => setAddress(e.target.value)}
-        placeholder="Enter Ethereum address here"
-      />
-      <button onClick={() => fetchNFTs()}>Fetch NFTs</button>
-      {fetchError && <p style={{ color: 'red' }}>{fetchError}</p>}
-      <DisplayMyNfts fetchNFTs={fetchNFTs} />
-    </div>
-  );
+	  return (
+		<div>
+		  {account ? (
+			<>
+			  <h1>Your NFTs: {account}</h1>
+			  <button onClick={() => fetchNFTs()}>Fetch NFTs</button>
+			  {fetchError && <p style={{ color: 'red' }}>{fetchError}</p>}
+			  {/* <DisplayMyNfts fetchNFTs={fetchNFTs} /> */}
+			</>
+		  ) : (
+			<div>Connect your wallet</div>
+		  )}
+		</div>
+	  );
 };
 
 export default MyNftsPage;
