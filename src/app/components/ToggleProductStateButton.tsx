c2325b84535ff421b2c03f2d5aafee9d28a61838
Sam Acer <sam@sam.fr>
2024-04-24 23:48:37 +0200

    fix bug mobile

--- src/app/components/ToggleProductStateButton.tsx
+++ src/app/components/ToggleProductStateButton.tsx
@@ -16,9 +16,9 @@ const ToggleProductStateButton: React.FC<ToggleProductStateButtonProps> = ({
     <button
       className="bg-green-700 hover:bg-green-600 text-white font-bold py-4 px-8 rounded-lg shadow transition duration-200 ease-in-out transform hover:scale-105 block mx-auto my-4 text-xl"
       type="button"
-      onClick={() => handleChangeProductState(productStateValue === 0 ? 1 : 0)}
+      onClick={() => handleChangeProductState(productStateValue == 0 ? 1 : 0)}
     >
-      {productStateValue === 0 ? 'Déclarer volé' : 'Déclarer légit'}
+      {productStateValue == 0 ? 'Déclarer volé' : 'Déclarer légit'}
     </button>
   );
 };
